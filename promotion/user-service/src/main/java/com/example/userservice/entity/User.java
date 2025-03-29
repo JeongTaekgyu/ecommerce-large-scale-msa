57688f4a46bfba08ce15f2f1ee99f18f988e9ad7
JeongTaekgyu <taekgyu0602@gmail.com>
2025-03-29 15:50:28 +0900

    User와 UserLoginHistory 엔터티 및 repository 작성

---
+++ promotion/user-service/src/main/java/com/example/userservice/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.userservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    @JsonIgnore // 응답 값이 되면 안돼서?
+    private String passwordHash;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
+}
