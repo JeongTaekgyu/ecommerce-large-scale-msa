9aa82844f89cdcbd6188a30420f05e7809802c1c
JeongTaekgyu <taekgyu0602@gmail.com>
2025-03-29 16:33:28 +0900

    @Repository 어노테이션 등록함 (Spring Data JPA의 인터페이스(JpaRepository)를 상속 받아서 자동으로 빈으로 등록이 되지만 명시성,가독성을 높이기 위해 등록함)

--- promotion/user-service/src/main/java/com/example/userservice/repository/UserLoginHistoryRepository.java
+++ promotion/user-service/src/main/java/com/example/userservice/repository/UserLoginHistoryRepository.java
@@ -3,9 +3,11 @@ package com.example.userservice.repository;
 import com.example.userservice.entity.User;
 import com.example.userservice.entity.UserLoginHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserLoginHistoryRepository extends JpaRepository<UserLoginHistory, Integer> {
     List<UserLoginHistory> findByUserOrderByLoginTimeDesc(User user);
 }
