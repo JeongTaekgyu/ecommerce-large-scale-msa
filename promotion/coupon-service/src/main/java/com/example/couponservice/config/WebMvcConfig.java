7c7b66c8ed699ef8ee7a79ce7e3966dc759482a8
JeongTaekgyu <taekgyu0602@gmail.com>
2025-04-04 18:55:45 +0900

    WebMvcConfig 로 Spring MVC 인터셉터(UserIdInterceptor)를 쿠폰 관련 API 요청에만 적용하도록 설정

---
+++ promotion/coupon-service/src/main/java/com/example/couponservice/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.couponservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+    // UserIdInterceptor 인터셉터(Interceptor) 를 특정 경로에 적용한다.
+
+    private final UserIdInterceptor userIdInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userIdInterceptor)
+                .addPathPatterns("/api/**/coupons/**");
+    }
+}
