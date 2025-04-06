d17e5e06b1d139dcf9e03917e69696626ce97aea
JeongTaekgyu <taekgyu0602@gmail.com>
2025-04-06 18:11:33 +0900

    PageController 파일 위치 변경

--- promotion/coupon-service/src/main/java/com/example/couponservice/controller/PageController.java
+++ promotion/coupon-service/src/main/java/com/example/couponservice/controller/PageController.java
@@ -1,4 +1,4 @@
-package com.example.couponservice.controller.v1;
+package com.example.couponservice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
