17a9f729ae290ad5900cb0f52cc2237ad6331d0c
JeongTaekgyu <taekgyu0602@gmail.com>
2025-04-19 18:10:12 +0900

    point-service-batch 모듈 init

---
+++ promotion/point-service-batch/src/test/java/com/example/pointservicebatch/PointServiceBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pointservicebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointServiceBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
