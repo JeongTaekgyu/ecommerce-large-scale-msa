2813a62753a59545d146df175d6cdf0697102a45
JeongTaekgyu <taekgyu0602@gmail.com>
2025-04-30 16:27:39 +0900

    RedisConfig 설정

---
+++ promotion/time-sale-service/src/main/java/com/example/timesaleservice/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.example.timesaleservice.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    // useSingleServer 는 단일 Redis 서버에 연결하도록 설정한다.(클러스터나 센티넬 모드가 아닌 단일 노드).
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort);
+        return Redisson.create(config);
+    }
+}
