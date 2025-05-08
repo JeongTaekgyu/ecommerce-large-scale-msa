0f9d020db5fd19ed71d20287b08c07d03a1e4ecf
JeongTaekgyu <taekgyu0602@gmail.com>
2025-05-08 16:02:04 +0900

    point-service-batch를 h2에서 postgresql로 변경

--- promotion/point-service-batch/src/main/resources/schema.sql
+++ promotion/point-service-batch/src/main/resources/schema.sql
@@ -4,31 +4,29 @@ DROP TABLE IF EXISTS daily_point_reports;
 
 CREATE TABLE points
 (
-    id               BIGINT AUTO_INCREMENT,
+    id               BIGSERIAL PRIMARY KEY,
     user_id          BIGINT      NOT NULL,
     amount           BIGINT      NOT NULL,
     type             VARCHAR(20) NOT NULL,
     description      VARCHAR(255),
     balance_snapshot BIGINT      NOT NULL,
     created_at       TIMESTAMP,
-    updated_at       TIMESTAMP,
-    PRIMARY KEY (id)
+    updated_at       TIMESTAMP
 );
 
 CREATE TABLE point_balances
 (
-    id         BIGINT AUTO_INCREMENT,
+    id         BIGSERIAL PRIMARY KEY,
     user_id    BIGINT NOT NULL,
     balance    BIGINT NOT NULL DEFAULT 0,
     created_at TIMESTAMP,
     updated_at TIMESTAMP,
-    PRIMARY KEY (id),
     CONSTRAINT uk_point_balance_user_id UNIQUE (user_id)
 );
 
 CREATE TABLE daily_point_reports
 (
-    id            BIGINT AUTO_INCREMENT,
+    id            BIGSERIAL PRIMARY KEY,
     user_id       BIGINT NOT NULL,
     report_date   DATE   NOT NULL,
     earn_amount   BIGINT NOT NULL DEFAULT 0,
@@ -37,6 +35,48 @@ CREATE TABLE daily_point_reports
     net_amount    BIGINT NOT NULL DEFAULT 0,
     created_at    TIMESTAMP,
     updated_at    TIMESTAMP,
-    PRIMARY KEY (id),
     CONSTRAINT uk_daily_point_report UNIQUE (user_id, report_date)
 );
+
+-- DROP TABLE IF EXISTS points;
+-- DROP TABLE IF EXISTS point_balances;
+-- DROP TABLE IF EXISTS daily_point_reports;
+--
+-- CREATE TABLE points
+-- (
+--     id               BIGINT AUTO_INCREMENT,
+--     user_id          BIGINT      NOT NULL,
+--     amount           BIGINT      NOT NULL,
+--     type             VARCHAR(20) NOT NULL,
+--     description      VARCHAR(255),
+--     balance_snapshot BIGINT      NOT NULL,
+--     created_at       TIMESTAMP,
+--     updated_at       TIMESTAMP,
+--     PRIMARY KEY (id)
+-- );
+--
+-- CREATE TABLE point_balances
+-- (
+--     id         BIGINT AUTO_INCREMENT,
+--     user_id    BIGINT NOT NULL,
+--     balance    BIGINT NOT NULL DEFAULT 0,
+--     created_at TIMESTAMP,
+--     updated_at TIMESTAMP,
+--     PRIMARY KEY (id),
+--     CONSTRAINT uk_point_balance_user_id UNIQUE (user_id)
+-- );
+--
+-- CREATE TABLE daily_point_reports
+-- (
+--     id            BIGINT AUTO_INCREMENT,
+--     user_id       BIGINT NOT NULL,
+--     report_date   DATE   NOT NULL,
+--     earn_amount   BIGINT NOT NULL DEFAULT 0,
+--     use_amount    BIGINT NOT NULL DEFAULT 0,
+--     cancel_amount BIGINT NOT NULL DEFAULT 0,
+--     net_amount    BIGINT NOT NULL DEFAULT 0,
+--     created_at    TIMESTAMP,
+--     updated_at    TIMESTAMP,
+--     PRIMARY KEY (id),
+--     CONSTRAINT uk_daily_point_report UNIQUE (user_id, report_date)
+-- );
